29dc3e60a2a0efa8dbdd1fbceaa44009abe364dd
Kilian Paquier <kilian.paquier@outlook.fr>
2020-11-06 19:16:29 +0100

    modifs

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
 	selector: 'app-experiences',
 	templateUrl: './experiences.component.html',
 })
 export class ExperiencesComponent implements OnInit {
+	@ViewChildren(MatExpansionPanel) panels: QueryList<MatExpansionPanel>;
+
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	closeOthersPanels(panelNumber: number) {
+		this.panels.forEach(panel => {
+			if (panel.opened && !panel.id.toString().includes(panelNumber.toString())) panel.close();
+		});
+	}
 }
